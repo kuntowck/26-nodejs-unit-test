a3f2eada760e0257728e80d8d15fc8918a2e81d3
kuntowck <kunto312002@gmail.com>
2024-06-27 08:06:01 +0700

    console transport

--- tests/logger.test.js
+++ tests/logger.test.js
@@ -1,7 +1,12 @@
-import winston, { info } from "winston";
+/* eslint-disable no-undef */
+import winston from "winston";
 
-test("create new logger", () => {
-  const logger = winston.createLogger({});
+test("create new logger with console transport", () => {
+  // object logger
+  const logger = winston.createLogger({
+    // console transports
+    transports: [new winston.transports.Console({})],
+  });
 
   logger.log({
     level: "info",
